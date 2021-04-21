d02f0d5baa235b20e9623fcb8788faf0f8f3fdad
Torres <torresmiranda.rj@gmail.com>
2021-04-21 19:37:59 -0300

    Create promises.js

---
+++ ES Next/promises.js
@@ -0,0 +1,23 @@
+/**
+ * Promises são utilizadas quando há a vontade de se ter um processamento assíncrono.
+ * 
+ * Os "caminhos" mais naturais de uma Promise são:
+ * 1. A promessa ser cumprida
+ * 2. A promessa ser rejeitada
+ * 
+ * Aceitam apenas um único parâmetro
+*/
+
+function falarDepoisDe(segundos, frase) {
+    return new Promise((resolve, reject) => { // Resolver, rejeitar
+        setTimeout(() => {
+            resolve(frase) // A frase não será imprimida, ela será recebida a partir de uma Promise
+        }, segundos * 1000) // Esperar o tempo necessário, no caso, os segundos informados pelo usuário
+    })   
+}
+
+falarDepoisDe(3, 'Que legal!')
+    .then(frase => frase.concat('?!?'))
+    .then(outraFrase => console.log(outraFrase)) // Após 3 segundos, a frase Que legal!?!? foi imprimida
+
+// Promises tem a função then e esta é chamada quando a promise é resolvida passando o objeto desejado   
