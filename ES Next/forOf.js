4b7a97cecde52c5ee730d491038f735b9f743cbb
Torres <torresmiranda.rj@gmail.com>
2021-04-21 18:56:11 -0300

    Update forOf.js

--- ES Next/forOf.js
+++ ES Next/forOf.js
@@ -1,5 +1,4 @@
 /**
- * O laço for of itera sobre valores
  * O laço for of percorre dados iterativos (Array, Map, Set, arguments, etc),
  * chamando uma função personalizada com instruções a serem executadas para
  * o valor de cada objeto distinto. 
